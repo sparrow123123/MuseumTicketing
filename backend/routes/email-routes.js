a8faae001abc924fbf1e88fd0becfebcc6d7947f
sanjayp-khandan-M <msanjayp21@gmail.com>
2024-01-24 04:32:46 +0530

    first commit

--- backend/routes/email-routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendemail} from "../controllers/email/email-controller.js";
-
-const emailRouter = express.Router();
-
-emailRouter.post("/",sendemail);
-
-
-export default emailRouter;
